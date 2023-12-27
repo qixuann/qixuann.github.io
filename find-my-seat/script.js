ef65e342ab0bcb6843a20cd4233ef56092cc52d1
qixuann <qi_xuan_@hotmail.com>
2023-12-27 18:05:13 +0800

    v1.1
    1. Autofit function
    2. Dropdown box adjust to searchable

--- find-my-seat/script.js
+++ find-my-seat/script.js
@@ -1,13 +1,4 @@
-// Function to populate dropdown with names from seat.js
-function populateDropdown() {
-  var select = document.getElementById('name');
-  for (var name in seats) {
-    var option = document.createElement('option');
-    option.value = name;
-    option.textContent = name;
-    select.appendChild(option);
-  }
-}
+
 
 // Function to handle radio button selection
 function handleDrivingSelection() {
@@ -32,6 +23,34 @@ function handleDrivingSelection() {
 
 // Populate dropdown on page load and handle radio button selection
 window.onload = function() {
-  populateDropdown();
+  const nameInput = document.getElementById('name');
+  const nameList = document.getElementById('nameList');
+  const errorMessage = document.getElementById('errorMessage');
+
+  // Populate the datalist options from the seats object
+  Object.keys(seats).forEach(function(name) {
+    const option = document.createElement('option');
+    option.value = name;
+    nameList.appendChild(option);
+  });
+
+  // Enable Find My Seat button when a name is selected
+  nameInput.addEventListener('input', function() {
+    const findSeatBtn = document.getElementById('findSeatBtn');
+    const enteredName = nameInput.value;
+    const isNameValid = enteredName && seats[enteredName];
+
+    if (!isNameValid) {
+      nameInput.style.borderColor = 'red'; // Change border color to red
+      errorMessage.textContent = 'Please select an appropriate name.';
+      errorMessage.style.color = 'red';
+    } else {
+      nameInput.style.borderColor = ''; // Reset border color
+      errorMessage.textContent = '';
+    }
+
+    findSeatBtn.disabled = !isNameValid;
+  });
+  
   handleDrivingSelection();
 };
